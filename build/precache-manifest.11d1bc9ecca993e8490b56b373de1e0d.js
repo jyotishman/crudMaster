94ae5f6c8df37329aa0d5a06f28a6110ba1ad7a1
Jyotishman saikia <jyotishmansaikia@Jyotishmans-MacBook-Pro.local>
2019-10-30 00:53:44 +0530

    build folder added

---
+++ build/precache-manifest.11d1bc9ecca993e8490b56b373de1e0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a533bb7fe87b0734516b8a4a32b67af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29d369ad0f4b40dcd0a2",
+    "url": "/static/css/main.5bd23d6f.chunk.css"
+  },
+  {
+    "revision": "f62bf368a012d7a8689b",
+    "url": "/static/js/2.06f2ec00.chunk.js"
+  },
+  {
+    "revision": "29d369ad0f4b40dcd0a2",
+    "url": "/static/js/main.faefa3b1.chunk.js"
+  },
+  {
+    "revision": "5a32f1494367b20dfe3f",
+    "url": "/static/js/runtime-main.9a4e0759.js"
+  }
+]);
